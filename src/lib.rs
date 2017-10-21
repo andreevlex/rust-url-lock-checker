7c01cc3d4ee505c4f2e16fa339f04049847f810b
Andreev Alexander <andreevlex.as@gmail.com>
2017-10-21 11:49:34 +0300

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate serde_derive;
 
+extern crate chrono;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 extern crate serde_urlencoded;
-extern crate chrono;
-extern crate reqwest;
 
 pub mod models;
 pub mod antizapret;
